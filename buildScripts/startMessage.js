f3ca6d8e21b5e03b6d8c2b76c0a273198985f3a8
Daniele Damiani <danieledamianiuk@gmail.com>
2017-10-03 08:03:23 +0100

    add eslint conf and tasks

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 // var chalk = require('chalk');
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); //eslint-disable-line no-console
