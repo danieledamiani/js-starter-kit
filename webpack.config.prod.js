169e9d8ce542dc2671973e0e76f8a63d3b2d0b4c
Daniele Damiani <danieledamianiuk@gmail.com>
2017-10-07 09:26:27 +0100

    add html manipulation plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,6 +16,23 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+    // create html file that includes reference to bundled JS
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
     // eliminates duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
     // Minify JS
