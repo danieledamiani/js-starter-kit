eaa703ba8aa480abd2fcd24ffa8a9cc61698ba88
Daniele Damiani <danieledamianiuk@gmail.com>
2017-10-07 09:13:49 +0100

    update function to get mock api query param

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,19 @@
 export default function getBaseUrl() {
-  const inDev = window.location.hostname === 'localhost';
-  return inDev ? 'http://localhost:3001/' : '/';
+  // const inDev = window.location.hostname === 'localhost';
+  // return inDev ? 'http://localhost:3001/' : '/';
+  return getQueryStringParamenterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParamenterByName(name, url) {
+  if(!url) { url = window.location.href; }
+
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regExp = '[?&]' + name + '(=([^&#]*)|&|#|$)',
+      regExpObj =  new RegExp(regExp),
+      results = regExpObj.exec(url);
+
+  if(!results) { return null; }
+  if(!results[2]) { return null; }
+
+  return decodeURIComponent(results[2].replace(/\+/g,  ' '));
 }
